b944b30b247bffce9292a96539f90d55253724e0
paulchen783 <57683775+paulchen783@users.noreply.github.com>
2019-11-13 14:27:16 -0500

    Add files via upload

---
+++ pythonprogram.py
@@ -0,0 +1,2 @@
+from pythonproject import*
+project()
---
+++ pythonproject.py
@@ -0,0 +1,95 @@
+import turtle
+bob = turtle.Turtle()
+turtle.colormode(255)
+bob.speed(0)
+turtle.bgcolor(0,0,0)
+bob.penup()
+bob.goto(-50,-50)
+bob.pendown()
+
+
+for times in range(25):
+    bob.color(255,0,255)
+    bob.forward(100)
+    bob.left(50)
+
+bob.penup()
+bob.goto(175,300)
+bob.pendown()
+    
+for times in range(50):
+    bob.color(127,0,255)
+    bob.forward(250)
+    bob.left(50)
+
+bob.penup()
+bob.goto(150,250)
+bob.pendown()
+
+for times in range(50):
+    bob.color(153,255,51)
+    bob.forward(150)
+    bob.left(110)
+
+bob.penup()
+bob.goto(125,225)
+bob.pendown()
+
+for times in range(50):
+    bob.color(153,255,51)
+    bob.forward(150)
+    bob.left(110)
+
+bob.penup()
+bob.goto(100,-25)
+bob.pendown()
+
+for times in range(50):
+    bob.color(153,255,51)
+    bob.forward(150)
+    bob.left(110)
+
+bob.penup()
+bob.goto(150,-175)
+bob.pendown()
+
+for times in range(50):
+    bob.color(153,255,51)
+    bob.forward(150)
+    bob.left(110)
+
+bob.penup()
+bob.goto(40,-65)
+bob.pendown()
+
+for times in range(50):
+    bob.color(153,255,51)
+    bob.forward(150)
+    bob.left(110)
+
+bob.penup()
+bob.goto(-225,-15)
+bob.pendown()
+
+for times in range(50):
+    bob.color(153,255,51)
+    bob.forward(150)
+    bob.left(110)
+
+bob.penup()
+bob.goto(-225,0)
+bob.pendown()
+
+for times in range(50):
+    bob.color(153,255,51)
+    bob.forward(150)
+    bob.left(110)
+
+bob.penup()
+bob.goto(0,200)
+bob.pendown()
+
+for times in range(50):
+    bob.color(153,255,51)
+    bob.forward(150)
+    bob.left(110)
